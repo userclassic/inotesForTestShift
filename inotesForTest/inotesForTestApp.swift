4810a588a537df081e6c1af9e27b24a70223edef
VA <VA>
2022-03-17 11:08:03 +0500

    Initial Commit

---
+++ inotesForTest/inotesForTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  inotesForTestApp.swift
+//  inotesForTest
+//
+//  Created by Владимир Осетров on 17.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct inotesForTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
